bba56b31a12a9deeea7fad2c47d08a46bc67dda7
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-12-07 23:27:00 +0100

    added user controller

--- LiteralnoUdruzenje/src/main/java/tim22/upp/LiteralnoUdruzenje/controller/UserController.java
+++ LiteralnoUdruzenje/src/main/java/tim22/upp/LiteralnoUdruzenje/controller/UserController.java
@@ -1,3 +1,37 @@
+package tim22.upp.LiteralnoUdruzenje.controller;
+
+import org.camunda.bpm.engine.*;
+import org.camunda.bpm.engine.form.FormField;
+import org.camunda.bpm.engine.form.TaskFormData;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import tim22.upp.LiteralnoUdruzenje.dto.FormFieldsDTO;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    IdentityService identityService;
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Autowired
+    TaskService taskService;
+
+    @Autowired
 
     @GetMapping(path = "/regtask", produces = "application/json")
     public @ResponseBody FormFieldsDTO getFormFieldsReaderRegistration() {
@@ -18,3 +52,4 @@
         List<FormField> properties = taskFormData.getFormFields();
         return new FormFieldsDTO(task.getId(), pi.getId(), properties);
     }
+}
